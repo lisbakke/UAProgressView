17e3c19976ef8cb33ca73f6f4783e1e2782979c1
Ben Lisbakken <lisbakke@gmail.com>
2014-07-21 22:04:52 -0600

    My Progress View.

--- UAProgressView.m
+++ UAProgressView.m
@@ -30,45 +30,45 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 #pragma mark - Init
 
 - (id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self sharedSetup];
-    }
-    return self;
+  self = [super initWithFrame:frame];
+  if (self) {
+    [self sharedSetup];
+  }
+  return self;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        [self sharedSetup];
-    }
-    return self;
+  self = [super initWithCoder:aDecoder];
+  if (self) {
+    [self sharedSetup];
+  }
+  return self;
 }
 
 - (void)sharedSetup {
 	self.progressView = [[UACircularProgressView alloc] initWithFrame:self.bounds];
 	self.progressView.shapeLayer.fillColor = [UIColor clearColor].CGColor;
 	[self addSubview:self.progressView];
-	
+
 	[self resetDefaults];
 }
 
 - (void)resetDefaults {
-	
+
 	self.fillChangedBlock = nil;
 	self.didSelectBlock	= nil;
 	self.progressChangedBlock = nil;
 	self.centralView = nil;
-	
+
 	_fillOnTouch = YES;
 	_progress = 0.0;
 	_animationDuration = 0.3f;
-	
+
 	self.borderWidth = 1.0f;
 	self.lineWidth = 2.0f;
-	
+
 	[self setupGestureRecognizer];
-	
+
 	[self tintColorDidChange];
 }
 
@@ -103,19 +103,19 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 #pragma mark - Color
 
 - (void)tintColorDidChange {
-    [super tintColorDidChange];
-	
-    UIColor *tintColor = self.tintColor;
-	
-    self.progressView.shapeLayer.strokeColor = tintColor.CGColor;
-    self.progressView.shapeLayer.borderColor = tintColor.CGColor;
+  [super tintColorDidChange];
+
+  UIColor *tintColor = self.tintColor;
+
+  self.progressView.shapeLayer.strokeColor = tintColor.CGColor;
+  self.progressView.shapeLayer.borderColor = [tintColor colorWithAlphaComponent:.5f].CGColor;
 }
 
 #pragma mark - Layout
 
 - (void)layoutSubviews {
-    [super layoutSubviews];
-    
+  [super layoutSubviews];
+
 	self.progressView.frame = self.bounds;
 	self.centralView.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
 }
@@ -123,25 +123,25 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 #pragma mark - Progress Control
 
 - (void)setProgress:(float)progress animated:(BOOL)animated {
-    
+
 	progress = MAX( MIN(progress, 1.0), 0.0); // keep it between 0 and 1
-	
+
 	if (_progress == progress) {
 		return;
 	}
-	
+
 	if (animated) {
-		
+
 		[self animateToProgress:progress];
-		
+
 	} else {
-		
+
 		[self stopAnimation];
 		_progress = progress;
 		[self.progressView updateProgress:_progress];
-		
+
 	}
-	
+
 	if (self.progressChangedBlock) {
 		self.progressChangedBlock(self, _progress);
 	}
@@ -154,49 +154,49 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 - (void)setAnimationDuration:(CFTimeInterval)animationDuration {
 	if (_animationDuration < 0)
 		return;
-	
-    _animationDuration = animationDuration;
+
+  _animationDuration = animationDuration;
 }
 
 - (void)animateToProgress:(float)progress {
-    [self stopAnimation];
-    
-    // Add shape animation
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    animation.duration = self.animationDuration;
-    animation.fromValue = @(self.progress);
-    animation.toValue = @(progress);
-    animation.delegate = self;
-    [self.progressView.layer addAnimation:animation forKey:UAProgressViewProgressAnimationKey];
-    
-    // Add timer to update valueLabel
+  [self stopAnimation];
+
+  // Add shape animation
+  CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+  animation.duration = self.animationDuration;
+  animation.fromValue = @(self.progress);
+  animation.toValue = @(progress);
+  animation.delegate = self;
+  [self.progressView.layer addAnimation:animation forKey:UAProgressViewProgressAnimationKey];
+
+  // Add timer to update valueLabel
 	_valueLabelProgressPercentDifference = (progress - self.progress) * 100;
-    CFTimeInterval timerInterval =  self.animationDuration / ABS(_valueLabelProgressPercentDifference);
-    self.valueLabelUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:timerInterval
-                                                                  target:self
-                                                                selector:@selector(onValueLabelUpdateTimer:)
-                                                                userInfo:nil
-                                                                 repeats:YES];
-    
-    
-    _progress = progress;
+  CFTimeInterval timerInterval =  self.animationDuration / ABS(_valueLabelProgressPercentDifference);
+  self.valueLabelUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:timerInterval
+                                                                target:self
+                                                              selector:@selector(onValueLabelUpdateTimer:)
+                                                              userInfo:nil
+                                                               repeats:YES];
+
+
+  _progress = progress;
 }
 
 - (void)stopAnimation {
-    // Stop running animation
-    [self.progressView.layer removeAnimationForKey:UAProgressViewProgressAnimationKey];
-    
-    // Stop timer
-    [self.valueLabelUpdateTimer invalidate];
-    self.valueLabelUpdateTimer = nil;
+  // Stop running animation
+  [self.progressView.layer removeAnimationForKey:UAProgressViewProgressAnimationKey];
+
+  // Stop timer
+  [self.valueLabelUpdateTimer invalidate];
+  self.valueLabelUpdateTimer = nil;
 }
 
 - (void)onValueLabelUpdateTimer:(NSTimer *)timer {
-    if (_valueLabelProgressPercentDifference > 0) {
-        _valueLabelProgressPercentDifference--;
-    } else {
-        _valueLabelProgressPercentDifference++;
-    }
+  if (_valueLabelProgressPercentDifference > 0) {
+    _valueLabelProgressPercentDifference--;
+  } else {
+    _valueLabelProgressPercentDifference++;
+  }
 }
 
 #pragma mark - Highlighting
@@ -205,7 +205,7 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 	if (self.fillOnTouch) {
 		// update the layer model
 		self.progressView.layer.backgroundColor = [self tintColor].CGColor;
-		
+
 		// call block
 		if (self.fillChangedBlock) {
 			self.fillChangedBlock(self, YES, NO);
@@ -215,7 +215,7 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 
 - (void)removeFillAnimated:(BOOL)animated {
 	if (self.fillOnTouch) {
-		
+
 		// add the fade-out animation
 		if (animated) {
 			CABasicAnimation *highlightAnimation = [CABasicAnimation animationWithKeyPath:@"backgroundColor"];
@@ -224,10 +224,10 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 			highlightAnimation.removedOnCompletion = NO;
 			[self.progressView.layer addAnimation:highlightAnimation forKey:@"backgroundColor"];
 		}
-		
+
 		// update the layer model.
 		self.progressView.layer.backgroundColor = [UIColor clearColor].CGColor;
-		
+
 		// call block
 		if (self.fillChangedBlock) {
 			self.fillChangedBlock(self, NO, animated);
@@ -242,63 +242,63 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 #pragma mark - CAAnimationDelegate
 
 - (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
-    [self.progressView updateProgress:_progress];
-    [self.valueLabelUpdateTimer invalidate];
-    self.valueLabelUpdateTimer = nil;
+  [self.progressView updateProgress:_progress];
+  [self.valueLabelUpdateTimer invalidate];
+  self.valueLabelUpdateTimer = nil;
 }
 
 #pragma mark - Gesture Recognizers
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
-    if (self.centralView && [touch.view isDescendantOfView:self.centralView] && self.centralView.userInteractionEnabled) {
+  if (self.centralView && [touch.view isDescendantOfView:self.centralView] && self.centralView.userInteractionEnabled) {
 		return NO;
-    }
-	
-    return YES;
+  }
+
+  return YES;
 }
 
 - (void)touchDetected:(UILongPressGestureRecognizer *)gestureRecognizer {
-	
+
 	CGPoint touch = [gestureRecognizer locationOfTouch:0 inView:self];
-	
+
 	if (UIGestureRecognizerStateBegan == gestureRecognizer.state) {	// press is being held down
-		
+
 		[self addFill];
-		
+
 	} else if (UIGestureRecognizerStateChanged == gestureRecognizer.state) {	// press was recognized, but then moved
-		
+
 		if (CGRectContainsPoint(self.bounds, touch)) {
-			
+
 			[self addFill];
-			
+
 		} else {
-			
+
 			[self removeFillAnimated:NO];
-			
+
 		}
-		
-    } else if (UIGestureRecognizerStateEnded == gestureRecognizer.state) { // the touch has been picked up
-		
+
+  } else if (UIGestureRecognizerStateEnded == gestureRecognizer.state) { // the touch has been picked up
+
 		if (CGRectContainsPoint(self.bounds, touch)) {
-			
+
 			[self removeFill];
-			
+
 			if (self.didSelectBlock) {
 				self.didSelectBlock(self);
 			}
-			
+
 		} else {
-			
+
 			[self removeFillAnimated:NO];
-			
+
 		}
-		
-    } else {
-		
+
+  } else {
+
 		[self removeFillAnimated:NO];
-		
+
 	}
-	
+
 }
 
 @end
@@ -308,44 +308,44 @@ NSString * const UAProgressViewProgressAnimationKey = @"UAProgressViewProgressAn
 @implementation UACircularProgressView
 
 + (Class)layerClass {
-    return CAShapeLayer.class;
+  return CAShapeLayer.class;
 }
 
 - (CAShapeLayer *)shapeLayer {
-    return (CAShapeLayer *)self.layer;
+  return (CAShapeLayer *)self.layer;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame {
-    if (self = [super initWithFrame:frame]) {
+  if (self = [super initWithFrame:frame]) {
 		[self updateProgress:0];
-    }
-	
-    return self;
+  }
+
+  return self;
 }
 
 - (void)layoutSubviews {
 	[super layoutSubviews];
-	
+
 	self.shapeLayer.cornerRadius = self.frame.size.width / 2.0f;
-    self.shapeLayer.path = [self layoutPath].CGPath;
+  self.shapeLayer.path = [self layoutPath].CGPath;
 }
 
 - (UIBezierPath *)layoutPath {
-    const double TWO_M_PI = 2.0 * M_PI;
-    const double startAngle = 0.75 * TWO_M_PI;
-    const double endAngle = startAngle + TWO_M_PI;
-    
-    CGFloat width = self.frame.size.width;
+  const double TWO_M_PI = 2.0 * M_PI;
+  const double startAngle = 0.75 * TWO_M_PI;
+  const double endAngle = startAngle + TWO_M_PI;
+
+  CGFloat width = self.frame.size.width;
 	CGFloat borderWidth = self.shapeLayer.borderWidth;
-    return [UIBezierPath bezierPathWithArcCenter:CGPointMake(width/2.0f, width/2.0f)
-                                          radius:width/2.0f - borderWidth - 0.5
-                                      startAngle:startAngle
-                                        endAngle:endAngle
-                                       clockwise:YES];
+  return [UIBezierPath bezierPathWithArcCenter:CGPointMake(width/2.0f, width/2.0f)
+                                        radius:width/2.0f - (borderWidth / 2.f)
+                                    startAngle:startAngle
+                                      endAngle:endAngle
+                                     clockwise:YES];
 }
 
 - (void)updateProgress:(float)progress {
-    [self updatePath:progress];
+  [self updatePath:progress];
 }
 
 - (void)updatePath:(float)progress {
